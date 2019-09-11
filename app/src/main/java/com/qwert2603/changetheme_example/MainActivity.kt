6263e9ca96d7f1ea8dfdebdb498060a78b51328c
alex <qwert2603@mail.ru>
2019-09-11 14:49:32 +0700

    \

--- app/src/main/java/com/qwert2603/changetheme_example/MainActivity.kt
+++ app/src/main/java/com/qwert2603/changetheme_example/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        R.layout.chth_fragment_change_theme
+        R.layout.chth_view_change_theme
+        R.layout.chth_include_change_theme
+
+        R.string.chth_app_theme_dark
+        R.string.chth_app_theme_light
     }
 }
